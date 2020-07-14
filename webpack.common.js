dde5f82e8b8d98001dbfd5a7c569c410156e4731
pmallol <pmallol@manas.tech>
2020-07-14 14:29:51 -0300

    Add dockerized Middleman

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    site: ['./source/javascripts/site.js'],
+    style: ['./source/stylesheets/site.css.scss'],
+  },
+  output: {
+    path: path.resolve(__dirname, '.tmp/dist'),
+    filename: '[name].min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
