9222ff725db79d5f5071ecea8827f69c16bf4d4e
Jonathan Kicillof <jkicillof@manas.com.ar>
2020-08-11 12:23:42 -0300

    style fixes

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -2,3 +2,27 @@ function handleScroll(e) {
     document.querySelector('.nav-bar').classList.toggle('scrolled', window.scrollY > 0)
 }
 window.addEventListener('scroll', handleScroll)
+
+function showVideo() {
+    const videoPlayer = document.querySelector('.videoPlayer')
+    const media = videoPlayer.querySelector('video')
+    videoPlayer.style.display = 'flex'
+    videoPlayer.addEventListener('click', hideVideo)
+    media.play()
+    document.body.style.overflow = 'hidden'
+}
+
+function hideVideo(e) {
+    if(e.target.tagName !== 'VIDEO') {
+        const videoPlayer = document.querySelector('.videoPlayer')
+        const media = videoPlayer.querySelector('video')
+        videoPlayer.style.display = 'none'
+        videoPlayer.removeEventListener('click', hideVideo)
+        media.pause()
+        document.body.style.overflow = 'unset'
+    }
+}
+
+function scrollTo(id) {
+    document.getElementById(id).scrollIntoView({behavior:'smooth'})
+}
